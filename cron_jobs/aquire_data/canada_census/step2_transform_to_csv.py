3cb558c9dbc47ff75b6ce0c57b46f0c2f118825c
suffer-sami <sami_email@protonmail.com>
2025-01-01 05:03:34 +0500

    selective canada census data according to column mapping

--- cron_jobs/aquire_data/canada_census/step2_transform_to_csv.py
+++ cron_jobs/aquire_data/canada_census/step2_transform_to_csv.py
@@ -2,6 +2,16 @@ import pandas as pd
 import os
 from pathlib import Path
 
+column_name_mapping = {
+    'LAT': 'latitude',
+    'LON': 'longitude',
+    '1-Population, 2021-Total Count': 'population_2021',
+    '6-Population density per square kilometre-Total Count': 'population_density_per_sq_km',
+    '39-Average age of the population-Total Count': 'average_age',
+    '50-Total - Private households by household size - 100% data-Total Count': 'total_private_households_by_size',
+    '318-  Median total income in 2020 ($)-Total Count': 'median_total_income_2020',
+}
+
 def process_csv_files():
     # Get the current directory where the script is located
     current_dir = Path(__file__).parent
@@ -21,16 +31,18 @@ def process_csv_files():
             # Read CSV 
             df = pd.read_csv(csv_file, low_memory=False, encoding='latin-1')
             
-            # rename latitude/longitude columns
-            column_mapping = {}
-            for col in df.columns:
-                if 'LAT' == col:
-                    column_mapping[col] = 'latitude'
-                elif 'LON' == col:
-                    column_mapping[col] = 'longitude'
+            # First check if all required columns exist in the dataframe
+            missing_columns = [col for col in column_name_mapping.keys() if col not in df.columns]
+            if missing_columns:
+                raise ValueError(f"Missing columns: {missing_columns}")
             
-            df = df.rename(columns=column_mapping)
+            # rename columns and only keep the columns that are in the column_name_mapping
+            df = df.rename(columns=column_name_mapping)
+            df = df[column_name_mapping.values()]
+            
+            # Append the processed dataframe to all_dfs
             all_dfs.append(df)
+            
             print(f"Successfully processed {csv_file.name}")
             
         except Exception as e:
