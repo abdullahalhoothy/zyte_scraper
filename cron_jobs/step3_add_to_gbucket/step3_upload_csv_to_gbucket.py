a01e5b0476c1773433e88f132ff6f96a90acc52e
Sharif Derhem <sharifderhem@gmail.com>
2025-05-02 17:54:45 +0300

    Update step3_upload_csv_to_gbucket.py to upload csv and json

--- cron_jobs/step3_add_to_gbucket/step3_upload_csv_to_gbucket.py
+++ cron_jobs/step3_add_to_gbucket/step3_upload_csv_to_gbucket.py
@@ -2,14 +2,14 @@ from datetime import datetime
 import sys
 import os
 from typing import List, Union, Dict
-
+import json
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 from common_methods import GCPBucketManager
 from cron_jobs.aquire_data.saudi_real_estate.load_config import load_config
 import glob
 
 
-def upload_csv_to_gcp(
+def upload_files_to_gcp(
     directories: List[Union[str, Dict]], gcp_manager: GCPBucketManager
 ) -> bool:
     """
@@ -32,18 +32,34 @@ def upload_csv_to_gcp(
 
             elif isinstance(directory, dict):
                 # Handle nested directories
-                for main_dir, sub_dirs in directory.items():
-                    for sub_dir in sub_dirs:
-                        csv_files = glob.glob(
-                            os.path.join(
-                                "cron_jobs/aquire_data", main_dir, sub_dir, "*.csv"
-                            )
-                        )
-                        for csv_file in csv_files:
-                            file_name = os.path.basename(csv_file)
-                            # Use the sub_dir (innermost folder) name in the GCS path
-                            gcs_path = f"{base_path}/{sub_dir}/{date}/{file_name}"
-                            gcp_manager.upload_csv(csv_file, gcs_path)
+               for main_dir, sub_dirs in directory.items():
+                for sub_dir in sub_dirs:
+                    population_json_dir = os.path.join(
+                        "cron_jobs/aquire_data", main_dir, sub_dir, "population_json_files"
+                    )
+                    csv_files = glob.glob(os.path.join("cron_jobs/aquire_data",main_dir , sub_dir,"*.csv"))
+                    for csv_file in csv_files:
+                        file_name = os.path.basename(csv_file)
+                        gcs_path = f"{base_path}/{sub_dir}/{date}/{file_name}"
+                        gcp_manager.upload_csv(csv_file , gcs_path)
+                    if not os.path.exists(population_json_dir):
+                        print(f"Skipped: {population_json_dir} does not exist.")
+                        continue
+
+                    for root, _, files in os.walk(population_json_dir):
+                        for file in files:
+                            local_file_path = os.path.join(root, file)
+                            file_name = os.path.basename(local_file_path)
+
+                            # Get folder inside population_json_files
+                            folder_to_upload_name = os.path.relpath(root, population_json_dir).split(os.sep)[0]
+
+                            # Build GCS path
+                            gcs_path = f"{base_path}/{sub_dir}/{date}/{folder_to_upload_name}/{file_name}"
+
+                            # Upload
+                            gcp_manager.upload_csv(local_file_path, gcs_path)
+
 
         return True
 
@@ -54,25 +70,34 @@ def upload_csv_to_gcp(
 
 config = load_config()
 # Initialize GCP manager once
+
+
+with open("cron_jobs/secrets_database.json", "r") as f:
+    secrets = json.load(f)
+
 gcp_manager = GCPBucketManager(
     bucket_name="s-locator",
-    credentials_path="cron_jobs/weighty-gasket-437422-h6-e1255469595a.json",
+    credentials_path=secrets["s-locator"]["bucket"]["credentials_path"],
 )
-
+gcp_manager_dev = GCPBucketManager(
+    bucket_name="dev-s-locator",
+    credentials_path=secrets["dev-s-locator"]["bucket"]["credentials_path"],
+)
 directories = [
     # "generate_economic_slocator_data",
     # "generate_housing_slocator_data",
     # "generate_household_slocator_data",
-    "saudi_real_estate",
+    # "saudi_real_estate",
     # "canada_census",
     # "canada_commercial_properties",
     # "saudi_census",
     # "saudi_ggl_categories_full_data"
-    # {"saudi_census": [
-        # "population",
-        #   "housing", 
-        #   "household"
-        #   ]},
+    {"saudi_census": [
+        "population",
+          "housing", 
+          "household"
+          ]},
 ]
 
-upload_success = upload_csv_to_gcp(directories, gcp_manager)
+upload_success = upload_files_to_gcp(directories, gcp_manager)
+upload_success_dev = upload_files_to_gcp(directories, gcp_manager_dev)
